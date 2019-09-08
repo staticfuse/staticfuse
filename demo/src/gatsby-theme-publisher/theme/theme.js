13289ee7ad69fdbc88903316410bdf56eaf32c14
Justin W Hall <justinwhall@me.com>
2019-09-07 21:12:32 -0600

    Setup theme component shadowing

---
+++ demo/src/gatsby-theme-publisher/theme/theme.js
@@ -0,0 +1,11 @@
+import theme from "../../../../gatsby-theme-publisher/src/theme/theme"
+
+const publisherTheme = {
+  ...theme,
+  colors: {
+    ...theme.colors,
+    // text: "blue",
+  },
+}
+
+export default publisherTheme
