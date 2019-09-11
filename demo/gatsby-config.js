01f975d0d6a1bf8fdbefb09c4088bb3406b45eda
Justin W Hall <justinwhall@me.com>
2019-09-11 11:39:24 -0600

    remove dupe wordPressUrl and fix twitter link

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -5,16 +5,15 @@ module.exports = {
     author: "Scott Bolinger",
     twitter: "@scottbolinger",
     siteUrl: `https://scottbolinger.com`,
-    wordPressUrl: `http://gatsby-preview.wtf`, // TODO: don't duplicate this field
   },
   plugins: [
     {
       resolve: `gatsby-theme-publisher`,
       options: {
-        menuName: `PRIMARY`, // Int - The ID of your main menu
-        mailChimpEndpoint: 0, // String - "https://example.us10.list-manage.com/subscribe/post?u=b9ef2fdd3edofhec04ba9b930&id=3l948gkt1d"
-        dynamicComments: 1, // Boolean - 1 or 2
-        gaTrackingId: 0, // String - GA tracking ID
+        menuName: `PRIMARY`,
+        mailChimpEndpoint: 0,
+        dynamicComments: 1,
+        gaTrackingId: 0,
         wordPressUrl: `http://gatsby-preview.wtf`,
       },
     },
