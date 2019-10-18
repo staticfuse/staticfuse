1f1c935b87673656a252fe7f45dab31a86a7558d
Justin W Hall <justinwhall@me.com>
2019-10-18 14:59:27 -0600

    eslint

--- gatsby-theme-publisher/create/createTags.js
+++ gatsby-theme-publisher/create/createTags.js
@@ -1,5 +1,6 @@
-const { BlogPreviewFragment } = require(`../src/templates/posts/data.js`)
-const tagTemplate = require.resolve(`../src/templates/tags/archive.js`)
+const { BlogPreviewFragment } = require('../src/templates/posts/data.js');
+
+const tagTemplate = require.resolve('../src/templates/tags/archive.js');
 
 module.exports = async ({ actions, graphql }) => {
   const GET_TAGS = `
@@ -25,36 +26,35 @@ module.exports = async ({ actions, graphql }) => {
       }
     }
     ${BlogPreviewFragment}
-  `
-  const { createPage } = actions
-  const allTags = []
-  const fetchTags = async variables =>
-    await graphql(GET_TAGS, variables).then(({ data }) => {
-      const {
-        wpgraphql: {
-          tags: {
-            nodes,
-            pageInfo: { hasNextPage, endCursor },
-          },
+  `;
+  const { createPage } = actions;
+  const allTags = [];
+  const fetchTags = async (variables) => graphql(GET_TAGS, variables).then(({ data }) => {
+    const {
+      wpgraphql: {
+        tags: {
+          nodes,
+          pageInfo: { hasNextPage, endCursor },
         },
-      } = data
-      nodes.map(tag => {
-        allTags.push(tag)
-      })
-      if (hasNextPage) {
-        return fetchTags({ first: 100, after: endCursor })
-      }
-      return allTags
-    })
+      },
+    } = data;
+    nodes.map((tag) => {
+      allTags.push(tag);
+    });
+    if (hasNextPage) {
+      return fetchTags({ first: 100, after: endCursor });
+    }
+    return allTags;
+  });
 
-  await fetchTags({ first: 100, after: null }).then(allTags => {
-    allTags.map(tag => {
-      console.log(`create tag: ${tag.slug}`)
+  await fetchTags({ first: 100, after: null }).then((tags) => {
+    tags.map((tag) => {
+      console.log(`create tag: ${tag.slug}`);
       createPage({
         path: `/tag/${tag.slug}`,
         component: tagTemplate,
         context: tag,
-      })
-    })
-  })
-}
+      });
+    });
+  });
+};
