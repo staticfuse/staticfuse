5f473f5452582117d7bc9d4354cd16a28a227d7f
Justin W Hall <justinwhall@me.com>
2019-10-27 13:07:41 -0600

    remove typo

--- packages/gatsby-theme-docs/src/layouts/DocLayout.js
+++ packages/gatsby-theme-docs/src/layouts/DocLayout.js
@@ -4,7 +4,6 @@ import { CSSReset } from '@chakra-ui/core';
 const DocsLayout = (props) => (
   <div className="staticfuse-docs-layout">
     <CSSReset />
-    <h1>from docs themejhjh</h1>
     {props.children}
   </div>
 );
