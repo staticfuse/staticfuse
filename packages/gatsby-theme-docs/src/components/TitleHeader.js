248cc1137c5732e70e0a2d2e5a0a5ec34a5cc1c2
Scott Bolinger <designbyscott@gmail.com>
2019-10-27 17:21:17 -0400

    fix docs responsive columns

--- packages/gatsby-theme-docs/src/components/TitleHeader.js
+++ packages/gatsby-theme-docs/src/components/TitleHeader.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Heading } from '@chakra-ui/core';
 
 const Toc = () => (
-  <Heading color="muted" fontWeight="200">Documentation</Heading>
+  <Heading color="muted" fontSize="1.75rem" fontWeight="200">Documentation</Heading>
 );
 
 export default Toc;
--- packages/gatsby-theme-docs/src/templates/doc.js
+++ packages/gatsby-theme-docs/src/templates/doc.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { graphql } from 'gatsby';
-import { Global } from '@emotion/core';
-import { Box, Grid } from '@chakra-ui/core';
-import Toc from '../components/Toc';
-import DocsLayout from '../layouts/DocLayout';
-import TitleHeader from '../components/TitleHeader';
+import React from "react";
+import { graphql } from "gatsby";
+import { Global } from "@emotion/core";
+import { Box } from "@chakra-ui/core";
+import Toc from "../components/Toc";
+import DocsLayout from "../layouts/DocLayout";
+import TitleHeader from "../components/TitleHeader";
 
-const Doc = (props) => {
+const Doc = props => {
   const post = props.data.markdownRemark;
 
   return (
@@ -14,26 +14,25 @@ const Doc = (props) => {
       <Global
         styles={{
           code: {
-            display: 'inline',
+            display: "inline"
           },
-          'pre code': {
-            display: 'block',
-          },
+          "pre code": {
+            display: "block"
+          }
         }}
       />
 
-      <Grid templateColumns="270px auto" gap={6}>
-        <Box>
+      <Box display={["block","block", "flex"]} className="docs-column-wrap">
+        <Box p={4} w={["100%", "100%", "30%"]} overflow="hidden">
           <TitleHeader />
           <Toc currentSlug={props.pageContext.slug} />
         </Box>
 
-        <Box h="auto" overflow="hidden">
+        <Box h="auto" w={["100%", "100%", "70%"]} overflow="hidden" p={4}>
           <h1>{post.frontmatter.title}</h1>
           <Box dangerouslySetInnerHTML={{ __html: post.html }} />
         </Box>
-      </Grid>
-
+      </Box>
     </DocsLayout>
   );
 };
